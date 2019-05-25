9a9997d51e93b1929b1d9e26181349f3a0c1d8a9
Igor Souza <igormcsouza@gmail.com>
2019-05-25 10:26:05 -0300

    Made understood the exemple

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-# Used for testes!!
+# Teste da aplicação pra ler excel e converter para csv.
 
 from app.exceltocsv import convert
 
+'''
+A função convert() recebe o nome do arquivo excel e o nome do arquivo csv a ser criado,
+ela não retorna nada, mas cria um arquivo no mesmo diretório com o nome desejado.
+
+Caso nenhum nome seja enviado como no exemplo abaixo, ele lê exatamente um arquivo chamado
+test.xlsx e exporta um outro arquivo chamado csvfile.csv
+'''
 convert()
